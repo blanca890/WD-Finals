4186a09b39c227e55565e3303ab4db407d29618e
blanca890 <202310839@fit.edu.ph>
2024-07-12 12:11:32 +0800

    -feat(landing page) add hero image, icon and css -feat(jquery) add jquery -refactor(landing page) h1 add animation

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//window scroll
+
+$(window).on("scroll", function() {
+    var scrollTop=$(window).scrollTop();
+    if(scrollTop >= 80){
+        $(".body").addClass("fixed-header");
+    }else{
+        $(".body").removeClass("fixed-header");
+    }
+
+});
+
+
+
+$(document).ready(function(){
+
+    const typed = new Typed('#type-it', {
+        strings: ['Designer', 'Developer', 'Freelancer', 'Photographer', 'Traveller', 'Blogger'],
+        typeSpeed: 100,
+        loop:true
+      });
+});
