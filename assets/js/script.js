b8f82e17a4ecceb20eda7199415ecd4185e48b25
blanca890 <202310839@fit.edu.ph>
2024-07-13 11:09:21 +0800

    feat(landing page) add card section,animation and add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
 //window scroll
 
-$(window).on("scroll", function() {
-    var scrollTop=$(window).scrollTop();
-    if(scrollTop >= 80){
-        $(".body").addClass("fixed-header");
-    }else{
-        $(".body").removeClass("fixed-header");
-    }
 
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop >= 80) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+    });
 });
-
 
 
 $(document).ready(function(){
 
     const typed = new Typed('#type-it', {
-        strings: ['Designer', 'Developer', 'Freelancer', 'Photographer', 'Traveller', 'Blogger'],
+        strings: ['Designer', 'BSIT-CST', 'Freelancer', 'Programmer', 'Developer',],
         typeSpeed: 100,
         loop:true
       });
